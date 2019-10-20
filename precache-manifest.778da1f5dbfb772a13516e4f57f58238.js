4d04794555e0e85a56b7d4b39885dc8881d07d93
siart89 <artyomreva@gmail.com>
2019-10-20 13:49:58 +0300

    Updates

---
+++ precache-manifest.778da1f5dbfb772a13516e4f57f58238.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d14bb0e040d557cb8ab3e0837c0beb9a",
+    "url": "/myShop_app/index.html"
+  },
+  {
+    "revision": "0a88614cbbe306c33c0c",
+    "url": "/myShop_app/static/js/2.2143b45d.chunk.js"
+  },
+  {
+    "revision": "48af0aaf29fd034ebcec",
+    "url": "/myShop_app/static/js/main.028dd236.chunk.js"
+  },
+  {
+    "revision": "71123ecaceae71490585",
+    "url": "/myShop_app/static/js/runtime-main.ed9678ee.js"
+  },
+  {
+    "revision": "710693381cd0c70b1760a7bea2ffa278",
+    "url": "/myShop_app/static/media/main_bg.71069338.jpg"
+  }
+]);
